bbf2f80ad6a2b58997e4c0dc04908f1dddcb5507
saluumaa <salumahassan02@gmail.com>
2023-06-20 15:02:58 +0300

    Add trim upto 10

--- person.rb
+++ person.rb
@@ -34,5 +34,5 @@ puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 
-capitalized_trimmed_person = TrimDecorator.new(capitalizedPerson)
+capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
--- trim_decorator.rb
+++ trim_decorator.rb
@@ -2,6 +2,6 @@ require './base_decorator'
 
 class TrimDecorator < Decorator
   def correct_name
-    @nameable.correct_name.strip
+    @nameable.correct_name[0..9]
   end
 end
